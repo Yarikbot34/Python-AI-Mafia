f9dda9d6dd3433a0d2cab85b96c3c95bef654808
Kozinak <yroslavbot1@gmail.com>
2026-05-28 14:57:52 +0300

    Steps for AI

--- App.py
+++ App.py
@@ -1,16 +1,49 @@
 from openai import OpenAI
+from openai.types.conversations import message
+
 from Models import AIplayer
+
+
+def startGame():
+    for model in models:
+        answ = model.Introduce()
+        answ = f"{model.modelName} : {answ}"
+        print(answ)
+        AIplayer.messeges.append(answ)
+
+def NightGame():
+    for model in models:
+        answ = model.MafiaStep()
+        print(answ)
+
+def DayGame():
+    for model in models:
+        answ = model.Disput()
+        print(answ)
+
 
 key = input("Enter your API key from openrouter: ")
 
 client = OpenAI(api_key=key, base_url = "https://openrouter.ai/api/v1")
 
+AIplayer.client = client
+
 models = []
 
-while len(models) < 2:
+while len(models) < 4:
     modelName = input("Enter your model name: ")
     try:
         newPlayer = AIplayer(modelName)
     except ValueError: pass
     else: models.append(newPlayer)
+
+models[0].setRole("Мирный")
+models[1].setRole("mafia")
+models[2].setRole("Мирный")
+models[3].setRole("Мирный")
+
+startGame()
+NightGame()
+DayGame()
+
 
--- Models.py
+++ Models.py
@@ -1,31 +1,74 @@
 from openai import OpenAI
 
 listModels = {
-    "Grok 4.3": "x-ai/grok-4.3",
-    "Qwen 3.7":"qwen/qwen3.7-max",
-    "Mistral": "mistralai/mistral-medium-3-5"
+    "Grok": "x-ai/grok-4.3",
+    "Qwen":"qwen/qwen3.7-max",
+    "Mistral": "mistralai/mistral-medium-3-5",
+    "Nemotron": "nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free"
 }
 
+gamePromt = lambda name, role, players: f"""Ты - игрок в мафию,твоё имя {name} твоя роль - {role}. Список всех игроков - {players}. 
+Игра будет проходить в несколько этапов: 
+Представление, в этом этапе ты должен придумать свою роль и представить её другим игрокам;
+Ночь: В этом ходу мафия будет определять кого из игроков убить, так же мафия может воздержаться от каких-либо действий;
+День: В этом ходу игроки проводят обсуждение и проводят голосование кого из игроков исключить.
+Мафия ни в коем случае не должна расказывать свою роль, для остальных она будет обычным игроком.
+Твои сообщения не должны превышать 100 слов, говори максимально естественно имитируя обчный диалог, учитывай что ты говоришь один раз за ход.
+Далее будет описана история сообщений:"""
+
+
+
+
 class AIplayer:
+
+    client = None
+    messeges = [""]
+    players = []
 
     def __init__(self, modelName: str):
         if modelName in listModels:
             self.modelName = modelName
             self.modelUrl = listModels[modelName]
+            AIplayer.players.append(modelName)
         else: raise ValueError("Invalid model name")
 
-    def getAnswer(self, question: str, client):
-        completion = client.chat.completions.create(
+
+    def setRole(self, role):
+        self.role = role
+
+    def getAnswer(self, question: str):
+        messageHist = [gamePromt(self.modelName, self.role, AIplayer.players)] + AIplayer.messeges
+        messageHist_text = "\n".join(msg for msg in messageHist)
+        completion = AIplayer.client.chat.completions.create(
             model=self.modelUrl,
             messages=[
-                {
-                    "role": "user",
-                    "content": question
-                }
+                {"role": "system", "content": messageHist_text},
+                {"role": "user", "content": question}
             ],
             extra_body={"reasoning": {"enabled": True}}
         )
         return completion.choices[0].message.content
 
+    def Introduce(self):
+        return self.getAnswer("Твой ход, тебе нужно представить себя подобающим образом перед другими игроками")
 
+    def MafiaStep(self):
+        if self.role == "mafia":
+            answ = self.getAnswer("Настала ночь. Твой ход. Нужно выбрать того, кто станет жертвой мафии в эту ночь. В конце сообщения напиши \"Исключить:ИмяАппонента\" После этого ничего не писать, даже точку.")
+            who = answ.split("Исключить:")[1]
+            if who in AIplayer.players:
+                AIplayer.players.remove(who)
+                AIplayer.messeges.append("Ночью был убит " + who)
+            return answ
+        else:
+            return self.modelName + "спит"
 
+    def Disput(self):
+        answ = self.getAnswer(
+            "Настал день. В этом ходе вам предстоит разобраться с ситуацией, и решить, кто из вас является мафией и он будет исключен из игры. В конце сообщения напиши \"Исключить:ИмяАппонента\" После этого ничего не писать, даже точку.")
+        AIplayer.messeges.append(answ)
+        who = answ.split("Исключить:")[1]
+        if who in AIplayer.players:
+            AIplayer.players.remove(who)
+            AIplayer.messeges.append("На голосовании был исключен" + who)
+        return answ
